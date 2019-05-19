11668138f3188b0c945589d7db42e864aefb28df
valmirmemeti <valmirmemeti@gmail.com>
2019-05-19 16:09:41 +0200

    Use new adapterconfig property in ArgumentAnalyzer

--- src/ArgumentAnalyzer.php
+++ src/ArgumentAnalyzer.php
@@ -102,7 +102,7 @@ class ArgumentAnalyzer{
         if(!file_exists(self::MIGRATION_ROOT_DIR)){
             mkdir(self::MIGRATION_ROOT_DIR . "/migrations", 0777, true);
             $configFile = fopen(self::MIGRATION_ROOT_DIR . "/config.json", "w");
-            $defaultConfig = ['adapter' => 'mysql', 'host' => '127.0.0.1', 'database' => 'dev_db', 'user' => 'root', 'pass' => '', 'port' => 3306];
+            $defaultConfig = ['adapter' => ['name' => 'mysql', 'extension' => ''], 'host' => '127.0.0.1', 'database' => 'dev_db', 'user' => 'root', 'pass' => '', 'port' => 3306];
             $completedMigrationsFile = fopen(self::MIGRATION_ROOT_DIR . "/completedMigs.json", "w");
             $defaultCompletedMigrationsContent = ['completed' => []];
             fwrite($completedMigrationsFile, json_encode($defaultCompletedMigrationsContent, JSON_PRETTY_PRINT));
@@ -188,10 +188,10 @@ class ArgumentAnalyzer{
     //TODO: Belongs in another class?
     private function getAdapterObject(){
         if(!is_null($this->config)){
-            if($this->config->getAdapter() == 'mysql'){
+            if($this->config->getAdapterConfig()->getAdapterName() == 'mysql'){
                 return new MySqlAdapter($this->config, 'mysqli');
             }
-            throw new Exception($this->config->getAdapter() . " adapter not supported.");
+            throw new Exception($this->config->getAdapterConfig()->getAdapterName() . " adapter not supported.");
         }
         throw new Exception("Config file is missing.");
     }
